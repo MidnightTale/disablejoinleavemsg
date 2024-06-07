a8d7cd86408af53550c3039c8e3b26a9cdf9596c
MidnightTale <midnighttale24@gmail.com>
2024-06-07 21:09:01 +0700

    ok

--- src/main/java/net/hynse/disablejoinleavemsg/Disablejoinleavemsg.java
+++ src/main/java/net/hynse/disablejoinleavemsg/Disablejoinleavemsg.java
@@ -1,17 +1,25 @@
 package net.hynse.disablejoinleavemsg;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Disablejoinleavemsg extends JavaPlugin {
+public final class Disablejoinleavemsg extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.setJoinMessage(null);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        event.setQuitMessage(null);
     }
 }
