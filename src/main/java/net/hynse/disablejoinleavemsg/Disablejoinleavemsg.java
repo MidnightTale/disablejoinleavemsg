8d3a0180d58e549e3b5b6456d77bb939a33ce0c6
MidnightTale <midnighttale24@gmail.com>
2024-06-07 21:08:49 +0700

    Initial commit

---
+++ src/main/java/net/hynse/disablejoinleavemsg/Disablejoinleavemsg.java
@@ -0,0 +1,17 @@
+package net.hynse.disablejoinleavemsg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Disablejoinleavemsg extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
